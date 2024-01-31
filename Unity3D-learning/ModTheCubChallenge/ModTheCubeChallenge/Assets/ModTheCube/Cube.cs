b222176113094b87258081f341e3303effaea2f3
Gabriel Bekier <gabrielbekier@gmail.com>
2024-01-31 16:49:32 +0100

    Lesson3 and end of Junior1

---
+++ Unity3D-learning/ModTheCubChallenge/ModTheCubeChallenge/Assets/ModTheCube/Cube.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+
+    private Material material; 
+    void Start()
+    {
+        transform.position = new Vector3(0,0,0);
+        
+        material = Renderer.material;
+
+        material.color = new Color(0, 0, 0);
+    }
+    
+    void Update()
+    {
+        float sinVal = Mathf.Sin(Time.time * 2) * 3;
+
+        float positionY = Jumping(sinVal);
+
+        float scaleY = Squish(sinVal);
+
+        float scaleX = Spread(sinVal);
+
+        float scaleZ = Spread(sinVal);
+
+        Vector3 finalPosition = new Vector3(0, positionY, 0);
+        Vector3 finalScale = new Vector3(scaleX, scaleY, scaleZ);
+        transform.position = finalPosition;
+        transform.localScale = finalScale;
+        material.color = Rainbow();
+    }
+
+    private float Jumping(float val)
+    {
+        if (val <= (Renderer.bounds.size.y)/2)
+        {
+            
+            return (Renderer.bounds.size.y)/2;
+        }
+
+        return val ;
+
+    }
+
+    private float Squish(float val)
+    {
+        if (val > 0)
+        {
+            return 1;
+        }
+    
+        return val >= -0.90 ? val + 1 : 0.1f ;
+    }
+
+    private float Spread(float val)
+    {
+        if (val > 0)
+        {
+            return 1;
+        }
+
+        return Mathf.Abs(val) + 1;
+    }
+
+    private Color Rainbow()
+    {
+        float red = Mathf.Sin(Time.time);
+        float blue = Mathf.Sin(Time.time + (Mathf.PI) / 3);
+        float green = Mathf.Sin(Time.time + (Mathf.PI * 2) / 3);
+
+        return new Color(red, green, blue);
+    }
+}
---
+++ Unity3D-learning/ModTheCubChallenge/ModTheCubeChallenge/Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,68 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void Cube_Start_mA3666DC006A5BDAE6F4B2B174C8861BBCA2CC97B (void);
+extern void Cube_Update_m92E30DF8DDE75DE867ED92B41FAE863601717D7E (void);
+extern void Cube_Jumping_m1A8B88A9B14304277FD549C39824E99526E9CB2C (void);
+extern void Cube_Squish_mBFFF34770F74936A1F30114613753FDCFEEE52AF (void);
+extern void Cube_Spread_m8F652C7771FF210467B002891ADACA8C66550244 (void);
+extern void Cube_Rainbow_m5B805984A8A892DEBD28B098EA4629F80B7D5F68 (void);
+extern void Cube__ctor_m6140AE68D7A41B6B2420D7C9BE28F59E2EE58292 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[9] = 
+{
+	Cube_Start_mA3666DC006A5BDAE6F4B2B174C8861BBCA2CC97B,
+	Cube_Update_m92E30DF8DDE75DE867ED92B41FAE863601717D7E,
+	Cube_Jumping_m1A8B88A9B14304277FD549C39824E99526E9CB2C,
+	Cube_Squish_mBFFF34770F74936A1F30114613753FDCFEEE52AF,
+	Cube_Spread_m8F652C7771FF210467B002891ADACA8C66550244,
+	Cube_Rainbow_m5B805984A8A892DEBD28B098EA4629F80B7D5F68,
+	Cube__ctor_m6140AE68D7A41B6B2420D7C9BE28F59E2EE58292,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[9] = 
+{
+	1102,
+	1102,
+	869,
+	869,
+	869,
+	1051,
+	1102,
+	2124,
+	1102,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	9,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
